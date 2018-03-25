becb56091cb9ac65d940f3f5fdf8064407646b8e
Tushar Tuteja <ttuteja@gmail.com>
2018-03-25 11:02:35 +0530

    minor commit

---
+++ model.py
@@ -0,0 +1,18 @@
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+
+model = Sequential()
+
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', 
+                        input_shape=(224, 224, 3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(GlobalAveragePooling2D())
+model.add(Dense(133, activation='softmax'))
+
+model.summary()
